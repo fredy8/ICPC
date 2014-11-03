5694c0c75be10cb1e08c75626996b6e6741419cc
Alfredo Altamirano <fredy.altamirano8@gmail.com>
2014-11-03 17:49:51 -0600

    fixed eulerian path

--- Handbook/Algorithms/Graphs/eulerian_path.h
+++ Handbook/Algorithms/Graphs/eulerian_path.h
@@ -5,12 +5,6 @@ void dfs2(Graph &g, list<int> &path, list<int>::iterator it, int cv) {
 			last = false;
 			edge->visited = 1;
 			g.edges[edge->to][edge->backEdge].visited = 1;
-			/*FORC(g.edges[edge->to], neighborEdge) {
-				if(neighborEdge->to == cv && !neighborEdge->visited) {
-					g.edges[edge->to][neighborEdge - g.edges[edge->to].begin()].visited = 1;
-					break;
-				}
-			}*/
 			dfs2(g, path, path.insert(it, cv), edge->to);
 		}
 	}
@@ -21,6 +15,8 @@ void dfs2(Graph &g, list<int> &path, list<int>::iterator it, int cv) {
 vi getEulerianPath(Graph &g/*undirected*/, int initial) {
 	list<int> path;
 	dfs2(g, path, path.begin(), initial);
+	if(g.edges[initial].size()%2 == 0)
+		path.pop_front();
 	vi p;
 	FORC(path, it)
 		p.pb(*it);
