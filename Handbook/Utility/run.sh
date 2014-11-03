bf385da1de88f658e8f7f0a2b1c096e27a5c3d18
Alfredo Altamirano <fredy.altamirano8@gmail.com>
2014-11-03 11:37:19 -0600

    Documentacion

--- Handbook/Utility/run.sh
+++ Handbook/Utility/run.sh
@@ -1,1 +1,3 @@
-clear;g++ $1 -op&&python -c"import re;print(re.search(r'^/\*\n((?s).*)\*/',open('$1').read()).group(1))"|./p;rm p
+clear;
+g++ $1 -op && python -c "import re;print(re.search(r'^/\*\n((?s).*)\*/',open('$1').read()).group(1))" | ./p;
+rm p
--- Handbook/Utility/string_number_conversion.h
+++
@@ -1,5 +0,0 @@
-template <typename T>
-string toString(T n) { ostringstream ss; ss << n; return ss.str(); }
-
-template <typename T>
-T toNum(const string &Text) { istringstream ss(Text); T result; return ss >> result ? result : 0; }
