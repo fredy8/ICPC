e6c5e477f42731f216da58990f0d037e951fcc7a
Salvador Pamanes <s.pamanes@gmail.com>
2014-11-05 08:16:23 -0600

    arreglar kmp off by one

--- Tests/run.sh
+++ Tests/run.sh
@@ -1,3 +1,3 @@
-# clear;g++ -o test test.cpp -I ../Handbook/ && ./test && rm test
-clear;g++ -g -o test test.cpp -I ../Handbook/ && gdb ./test #&& rm test
+clear;g++ -o test test.cpp -I ../Handbook/ && ./test && rm test
+# clear;g++ -g -o test test.cpp -I ../Handbook/ && gdb ./test #&& rm test
 
