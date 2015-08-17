6f531ae0812d6ce4e1ca291c896d93b42e2eb974
Alfredo Altamirano <fredy.altamirano8@gmail.com>
2015-08-17 12:28:32 -0500

    changed to compile with c++11 and added bits/stdc++ to header

--- workspace/problem.cpp
+++ workspace/problem.cpp
@@ -1,26 +1,8 @@
 /*
-6
-912311
+1 2
 */
-#include <algorithm>
-#include <bitset>
-#include <cmath>
-#include <cstdio>
-#include <cstring>
-#include <list>
-#include <iomanip>
-#include <iostream>
-#include <queue>
-#include <list>
-#include <map>
-#include <numeric>
-#include <unordered_map>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <utility>
-#include <vector>
-#include <cstdlib>
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0), cin.tie(0), cin.tie(0), cout.tie(0), cout.precision(15);
 #define INF 1000000000
 #define FOR(i, a, b) for(int i=int(a); i<int(b); i++)
 #define FORC(cont, it) for(typeof((cont).begin()) it=(cont).begin(); it!=(cont).end(); it++)
@@ -31,15 +13,8 @@ typedef int mint;
 #define int ll
 using namespace std; typedef long long ll; typedef pair<int, int> ii; typedef vector<int> vi; typedef vector<ii> vii; typedef vector<vi> vvi;
 
-mint main() {
-	int N;
-	cin >> N;
-	vi arr(N, 0);
-	int l = 0, r = 0;
-	set<int> possible;
-	FOR(i, 0, N) {
-		cin >> arr[i];
-		l += arr[i] == 1;	
-		if (l != r	
-	}
+mint main() { _
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
 }
--- workspace/run.sh
+++ workspace/run.sh
@@ -1,1 +1,2 @@
-clear;g++ -Wall -Wextra -pedantic -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fstack-protector $1 -op && python -c "import re; print(re.search(r'^/\*\n((?s).*)\*/', open('$1').read()).group(1))" | ./p; rm p
+clear
+g++ -std=c++11 -Wall -Wextra -pedantic -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fstack-protector $1 -op && python -c "import re; print(re.search(r'^/\*\n((?s).*)\*/', open('$1').read()).group(1))" | ./p && rm p
