5d11691c10c4c99b0d93b446d229f578c4c7fa8c
Alfredo Altamirano <fredy.altamirano8@gmail.com>
2015-08-17 12:40:31 -0500

    updated header to use bits/stdc++

--- Handbook/src/Utility/header.h
+++ Handbook/src/Utility/header.h
@@ -1,32 +1,20 @@
 /*
+1 2
 */
-#include <algorithm>
-#include <bitset>
-#include <cmath>
-#include <cstdio>
-#include <cstring>
-#include <deque>
-#include <iomanip>
-#include <iostream>
-#include <queue>
-#include <list>
-#include <map>
-#include <numeric>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <utility>
-#include <vector>
-#include <cstdlib>
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0), cin.tie(0), cin.tie(0), cout.tie(0), cout.precision(15);
 #define INF 1000000000
 #define FOR(i, a, b) for(int i=int(a); i<int(b); i++)
 #define FORC(cont, it) for(typeof((cont).begin()) it=(cont).begin(); it!=(cont).end(); it++)
 #define pb push_back
 #define mp make_pair
+#define all(x) (x).begin(), (x).end()
 typedef int mint;
 #define int ll
 using namespace std; typedef long long ll; typedef pair<int, int> ii; typedef vector<int> vi; typedef vector<ii> vii; typedef vector<vi> vvi;
 
-mint main() {
-	ios_base::sync_with_stdio(false);
+mint main() { _
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
 }
