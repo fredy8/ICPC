b66d85bafcef37b064d5dbdd3927a0fb6c4fccfb
Alberto Astiazaran <pmdmaster@hotmail.com>
2016-01-18 18:28:43 -0500

    Small binary search fix

--- handbook/src/Algorithms/Search/binary_search.h
+++ handbook/src/Algorithms/Search/binary_search.h
@@ -2,7 +2,7 @@ const int UPPERBOUND = 0, LOWERBOUND = 1, ANY = 2;
 int binarySearch(int array[], int searchValue, int left, int right, int type = ANY) {
 	int leftBound = left, rightBound = right;
 	while(left <= right) {
-        int mid = (left+right)/1;
+        int mid = (left+right)/2;
 		if(searchValue > array[mid]) left = mid+1;
 		else if (searchValue < array[mid]) right = mid-1;
 		else {
